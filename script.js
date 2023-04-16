e9b1c7674e2f5073cc151addb507966dda9eeb68
ALEJANDRA FERNÁNDEZ <alejafernandez28@gmail.com>
2023-04-16 09:11:06 -0300

    formularioReclamos

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const form = document.querySelector('formCotizador');
-
-form.addEventListener('submit', (event) => {
-  const servicio = document.getElementById('ingreseservicio').value;
-  const subservicio = document.getElementById('ingresesubservicio').value;
-  const formadepago = document.getElementById('formadepago').value;
- 
-  
-  if (ingreseservicio === '' || ingresesubservicio === '' || formadepago === '' ) {
-    alert('Por favor, complete todos los campos.');
-    event.preventDefault();
-  } else if (!/\S+@\S+\.\S+/.test(correo)) {
-    alert('Por favor, ingrese una dirección de correo electrónico válida.');
-    event.preventDefault();
-  }
-});
+//referenciar elementos del dom//
